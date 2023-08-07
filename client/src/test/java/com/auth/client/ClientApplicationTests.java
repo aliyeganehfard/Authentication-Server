0f8a44abbdf4e08adf81a759f99385fdc3da0d01
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2023-08-07 19:48:50 +0330

    Add client

---
+++ client/src/test/java/com/auth/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
