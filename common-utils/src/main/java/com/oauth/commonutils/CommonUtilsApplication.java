ac94ebbf6087a651f4252175bcfc0e2d077301a9
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2023-08-08 20:48:08 +0330

    Add common-utils

---
+++ common-utils/src/main/java/com/oauth/commonutils/CommonUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.oauth.commonutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonUtilsApplication.class, args);
+	}
+
+}
